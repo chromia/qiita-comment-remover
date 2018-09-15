8d5b1b3562a444d25dc2645544ce6b99da335a37
chromia <chromia@outlook.jp>
2018-09-16 03:49:55 +0900

    Qiita用に改変を加え、ライセンス/説明分を更新

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 説明
 ----
 
-togetterのまとめのコメントのうち、特定のユーザが作成したものをまとめて非表示にします。
+Qiitaのまとめのコメントのうち、特定のユーザが作成したものをまとめて非表示にします。
 
 非表示にするには、ユーザ名の隣の×をクリックします。
 [削除済]の表示をダブルクリックすると再表示します。
 
 
+※本スクリプトはrecyclebin5385様の「togecutter-alt」をベースとして作成しました。
+  この場を借りてお礼申し上げます。
+
+
 連絡先
 ------
-recyclebin5385[at]yahoo.co.jp ([at]を@に置換してください)
+メール: chromia[at]outlook.jp ([at]を@に置換してください)
+Github: https://github.com/chromia/qiita-comment-remover/issues
--- togecutter-alt.user.js
+++ togecutter-alt.user.js
@@ -1,26 +1,45 @@
 // ==UserScript==
-// @name        togecutter-alt
-// @namespace   recyclebin5385
-// @description togetterの特定ユーザのコメントを見えなくする
-// @include     https://togetter.com/li/*
-// @include     http://togetter.com/li/*
+// @name        qiita-comment-remover
+// @namespace   chromia
+// @description Qiitaの特定ユーザのコメントを見えなくする
+// @match       https://qiita.com/*/items/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @version     6
+// @version     1
 // @grant       none
 // ==/UserScript==
 
+//
+// 説明
+// ----
+//
+// Qiitaの記事に関するコメントのうち、特定のユーザが投稿したものを非表示にします。
+//
+// 非表示にするには、ユーザ名の隣の×をクリックします。
+// 削除済の表示をダブルクリックすると再表示します。
+//
+//
+// 連絡先
+// ------
+// chromia[at]outlook.jp ([at]を@に置換してください)
+//
+
+// オリジナルのソースコード
+// togecutter-alt
+// https://github.com/recyclebin5385/togecutter-alt/raw/master/togecutter-alt.user.js
+// ライセンス文(修正BSDライセンス)
+//
 // Copyright (c) 2017, recyclebin5385
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// * Redistributions of source code must retain the above copyright notice, 
+// * Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice, 
-//   this list of conditions and the following disclaimer in the documentation 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution.
-// * Neither the name of the <organization> nor the names of its contributors 
-//   may be used to endorse or promote products derived from this software 
+// * Neither the name of the <organization> nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
 //   without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
@@ -34,25 +53,42 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// 本スクリプトのライセンス(修正BSDライセンス, 派生元と同じ)
 //
-// 説明
-// ----
+// Copyright (c) 2018, chromia
+// All rights reserved.
 //
-// togetterのまとめのコメントのうち、特定のユーザが作成したものをまとめて非表示にします。
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the <organization> nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
 //
-// 非表示にするには、ユーザ名の隣の×をクリックします。
-// 削除済の表示をダブルクリックすると再表示します。
-//
-//
-// 連絡先
-// ------
-// recyclebin5385[at]yahoo.co.jp ([at]を@に置換してください)
-//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+//Cookieの名前
+var cookieName = "qcr_hiddenCommentUserIds";
+
 
 (function() {
     jQuery.noConflict();
     var $ = jQuery;
-    
+
+    //非表示ユーザーIDリストを格納したCookieの情報を取得する
     function getCookieMap() {
         var ret = new Array();
 
@@ -70,9 +106,10 @@
         return ret;
     }
 
+    //Cookieから非表示ユーザーIDリストを取得する
     function getHiddenUserIds() {
         var cookieMap = getCookieMap();
-        var joinedHiddenUserIds = cookieMap['hiddenCommentUserIds'];
+        var joinedHiddenUserIds = cookieMap[cookieName];
         if (joinedHiddenUserIds != null && joinedHiddenUserIds != '') {
             return joinedHiddenUserIds.split(' ');
         } else {
@@ -80,12 +117,14 @@
         }
     }
 
+    //非表示ユーザーIDリストをCookieに記録する
     function setHiddenUserIds(ids) {
         var now = new Date();
         var maxAgeDay = 366;
         now.setTime(now.getTime() + maxAgeDay * 24 * 60 * 60 * 1000);
         var expires = now.toGMTString();
-        var cookie = 'hiddenCommentUserIds=' + encodeURIComponent(ids.join(' ')) + ";expires=" + expires;
+        var path = "/";
+        var cookie = cookieName + '=' + encodeURIComponent(ids.join(' ')) + ";expires=" + expires + ";path=" + path;
 
         if (cookie.length > 4096) {
             return false;
@@ -96,6 +135,7 @@
         return true;
     }
 
+    //非表示ユーザーリストに指定ユーザーを追加する
     function addHiddenUserId(id) {
         var ids = getHiddenUserIds();
         if ($.inArray(id, ids) == -1) {
@@ -115,6 +155,7 @@
         }
     }
 
+    //非表示ユーザーリストから指定ユーザーを削除する
     function removeHiddenUserId(id) {
         var ids = getHiddenUserIds();
         var newIds = [];
@@ -126,21 +167,46 @@
         setHiddenUserIds(newIds);
     }
 
+    function getUserId(comment) {
+        //コメントのcommentHeader_creatorのdiv要素を取得
+        var creator = $(comment).find(".commentHeader_creator");
+        //その直下にあるアンカーを取得し、href属性(href='/userid'みたいになってる)からIDを取り出す
+        var idLinks = $(creator).children("a");
+        if(idLinks.length == 0){
+            return null;
+        }else{
+            var id = idLinks[0].getAttribute("href").replace(/\//, "");
+            return id;
+        }
+    }
+
+    //非表示ユーザーリストに含まれるユーザーのコメントを非表示にする
     function hideUsers() {
         var hiddenUserIds = getHiddenUserIds();
 
-        $("#comment_box .list_box").each(function(){
-            var listItem = $(this);
-            var idLink = $(this).find("a.status_name");
-            var id = idLink.text().replace(/^@/, "");
+        //コメントの表示要素をピックアップ
+        $(".commentList .comment").each(function(){
+            var comment = $(this);
+            //コメントの直下にあるHeaderとContentの要素を取得
+            var header = $(comment).find(".commentHeader");
+            var content = $(comment).find(".comment_content");
+            //ユーザーIDを取得
+            var id = getUserId(comment);
+            if(id == null) return;
+            console.log(id);
 
+            //非表示ユーザーIDリストと照らし合わせる
             if ($.inArray(id, hiddenUserIds) != -1) {
-                listItem.find(".list_tweet_box").hide();
-                if (listItem.find(".removed").length == 0) {
-                    $("<span>[削除済]</span>")
+                //入ってるので、コメント(のheaderとcontent)を非表示にする
+                console.log("remove:", id);
+                header.hide();
+                content.hide();
+                //その代わりに[削除済]表示を置く
+                if (comment.find(".removed").length == 0) {
+                    $("<div>[削除済]</div>")
                         .hide()
                         .addClass("removed")
-                        .css({"cursor": "pointer"})
+                        .css({"cursor": "pointer", "padding-bottom": "0.5em"})
 
                         .attr("title", id)
                         .dblclick(function() {
@@ -148,22 +214,29 @@
                                 removeHiddenUserId(id);
                             }
                         })
-                        .appendTo(listItem);
+                        .appendTo(comment);
                 }
-                listItem.find(".removed").show();
+                comment.find(".removed").show();
             } else {
-                listItem.find(".list_tweet_box").show();
-                listItem.find(".removed").hide();
+                //入ってないのでコメントを表示する
+                header.show();
+                content.show();
+                comment.find(".removed").hide();
             }
         });
     }
 
+    //ユーザー名の横に、[x](削除)ボタンを配置
     $(function() {
-        $("#comment_box .list_box").each(function(){
-            var listItem = $(this);
-            var idLink = listItem.find("a.status_name");
-            var id = idLink.text().replace(/^@/, "");
+        $(".commentList .comment").each(function(){
+            var comment = $(this);
+            //コメントのユーザー名の部分を取り出す
+            var username = $(comment).find(".commentHeader_publicUrlName");
+            //ユーザーIDを取得
+            var id = getUserId(comment);
+            if(id == null) return;
 
+            //ユーザー名の横にボタンを追加
             $("<span>[×]</span>")
                 .addClass("status_name")
                 .css({"cursor": "pointer"})
@@ -175,7 +248,7 @@
                     hideUsers();
                     return false;
                 })
-                .insertAfter(idLink);
+                .appendTo(username);
         });
 
         hideUsers();
